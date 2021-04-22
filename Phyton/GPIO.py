d55d5f00c2524b67c64452616b4d1990dac70cbf
KaitmazianMO <70343770+KaitmazianMO@users.noreply.github.com>
2021-04-22 16:58:44 +0300

    ADC task

--- Phyton/GPIO.py
+++ Phyton/GPIO.py
@@ -134,48 +134,120 @@ def sunVoltage (run_time, frequency, samplingFrequency):
         time.sleep (float (1/samplingFrequency))
     num2dac (0)
 
-sunVoltage (2, 50, 50)
+#sunVoltage (2, 50, 50)
 
 #first script
 
-print ('Writa a number (-1 for out)')
-input_num = int (input())
-while input_num != -1:
-    num2dac (input_num)
-    print ('Writa a number (-1 for out)')
-    input_num = int (input())
+#print ('Writa a number (-1 for out)')
+#input_num = int (input())
+#while input_num != -1:
+#    num2dac (input_num)
+#    print ('Writa a number (-1 for out)')
+#    input_num = int (input())
 
 num2dac (0)
 
 
 #second script
 
-print ('Writa a repetition number (-1 for out)')
-repetition_num = int (input())
-receptionNumber (repetition_num)
+#print ('Writa a repetition number (-1 for out)')
+#repetition_num = int (input())
+#receptionNumber (repetition_num)
+
+#num2dac (0)
+
+#zdef showSin (amplitude, sF):
+#    for i in amplitude:
+#        curr = int (round (i * 127) + 128)
+#        out = num2dac (curr)
+#        GPIO.output (Ds, out)
+#        time.sleep ( float (1 / int (sF))
+                    
+#freq = int (input ('Set frequency'))
+#sF   = int (input ('Set sampling frequency'))
+#t    = int (input ('Set time'))
+                    
+#tim       = np.arange (0, t, float (1/sF))
+#amplitude = np.sin (2*math.pi * tim*freq)
+#plt.plot (tim, amplitude)
+#plt.title ('sin')
+#plt.xlabel ('time')
+#plt.ylabel ('amplitude sin(time)')
+#plt.show()
+#show_sin (amplitude, sF)
+
+
+GPIO.setup (24, GPIO.IN)
+
+def dac_data (data):
+    for i in range (0, bit_depth):
+        GPIO.output (DAC[i], data[i])
+
+GPIO.setup (17, GPIO.OUT)
+GPIO.output (17, 1)
+
+
+#print ("Введите число (-1 для выхода)")
+
+#while True:
+#    num = int (input())
+#    if num == -1:
+#        break
+#    print (3.3 * float (num / 255))
+#    num2dac (num)
 
 num2dac (0)
 
-def showSin (amplitude, sF):
-    for i in amplitude:
-        curr = int (round (i * 127) + 128)
-        out = num2dac (curr)
-        GPIO.output (Ds, out)
-        time.sleep ( float (1 / int (sF))
-                    
-freq = int (input ('Set frequency'))
-sF   = int (input ('Set sampling frequency'))
-t    = int (input ('Set time'))
-                    
-tim       = np.arange (0, t, float (1/sF))
-amplitude = np.sin (2*math.pi * tim*freq)
-plt.plot (tim, amplitude)
-plt.title ('sin')
-plt.xlabel ('time')
-plt.ylabel ('amplitude sin(time)')
-plt.show()
-show_sin (amplitude, sF)
+cmpPin = 4
+GPIO.setup (cmpPin, GPIO.IN)
+voltage = 0
 
-
-
+def linearSearch():
+    for i in range (0, 255):
+        num2dac (i)
+        time.sleep (0.001)
+        if GPIO.input (cmpPin) == 0:
+            return i
             
+
+def binSearch():
+    valt = 0
+    step = 128
+    while step > 1:
+        #print (int (valt))
+        num2dac (int (valt))
+        time.sleep (0.001)
+        if GPIO.input (cmpPin) == 1:
+            valt += step
+        else: 
+            valt -= step
+        step /= 2
+    if valt < 0:
+        return 0
+    return int (valt) 
+
+#while True:
+#    currValt = binSearch()
+#    print (currValt, ':', float (3.3 * currValt / 255))
+
+
+
+GPIO.setup (21, GPIO.OUT)
+GPIO.setup (20, GPIO.OUT)
+GPIO.setup (16, GPIO.OUT)
+GPIO.setup (12, GPIO.OUT)
+GPIO.setup (7,  GPIO.OUT)
+GPIO.setup (8,  GPIO.OUT)
+GPIO.setup (25, GPIO.OUT)
+GPIO.setup (24, GPIO.OUT)
+
+while True:
+    currValt = binSearch() + 1
+    #print (currValt)
+    if currValt > 250:
+        currValt = 256
+    #print (currValt)
+    valtPow = int (math.log2 (currValt)) 
+    currValt = int (math.pow (2, valtPow)) - 1
+    #print (currValt, ':', float (3.3 * currValt / 255), 'V')
+    lightNumber (currValt)
